4852ad20b56033dd320cdd14fc73ce81bb99b989
yohhatu <yohhatu@gmail.com>
2012-06-25 06:48:02 +0900

    カレンダーのスタイリングをする(#5)

--- calendar.php
+++ calendar.php
@@ -1,5 +1,4 @@
 <?php
-
 // カレンダー
 $d = date("Y-m-d");
 
@@ -18,16 +17,22 @@ while ($fd--) {
     $s .= '<td></td>';
 }
 
-for ($i = 0; $i <= $ld; $i++) {
-    if( $y % 7 == 0 ) {
+for ($i = 1; $i <= $ld; $i++) {
+    if ($y % 7 == 0) {
         $s .= "</tr><tr>";
         $y = 0;
     }
     $s .= "<td>$i</td>";
-    $y ++;
+    $y++;
 }
 
-echo $s;
+$r = 7 - $y;
+
+while ($r--) {
+    $s .= "</tr><tr>";
+}
+
+$s .= "</tr></table>";
 
 ?>
 
@@ -36,9 +41,17 @@ echo $s;
     <head>
         <meta charset="UTF-8" />
         <title>PHPの練習</title>
+        <style>
+            td{
+                padding: 5px;
+                border: 1px solid #ccc;
+                text-align: right
+            }
+        </style>
     </head>
     <body>
         <h1>PHPの練習</h1>
-        <p>こんにちは。</p>
+        <p><?php echo $year; ?>年<?php echo $month; ?>月</p>
+        <?php echo $s; ?>
     </body>
 </html>
